e0196aa83d2d6124affd50edddd8c37cf4a8f0c0
Duramann <theo.dura@efrei.net>
2022-02-09 18:03:15 +0100

    Front, Back and docker

---
+++ back/server.js
@@ -0,0 +1,33 @@
+const PORT = 5000;
+const HOST = '0.0.0.0';
+const cors = require('cors');
+
+const express = require('express');
+
+const app = express();
+app.use(cors());
+
+const session = require('express-session');
+
+    app.use(
+        session({
+            secret: 'secret string',
+            resave: false,
+            saveUnitialized: false,
+            cookies :{}
+        })
+    );
+
+    app.get('/', function(req, res) {
+        if(!req.session.pageCountcurrent)
+            req.session.pageCountcurrent = 0;
+        req.session.pageCountcurrent++;
+        res.json({
+            pageCount: req.session.pageCountcurrent
+        });
+    });
+
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
---
+++ front/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from "react";
+
+function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:5000/")
+    .then((res) => res.json())
+    .then((data) => setData(data.pageCount))
+  }, []);
+
+  return (
+    <div className="App">
+      <h1> The page has been seen {data} times.</h1>
+    </div>
+  );
+}
+
+export default App;
